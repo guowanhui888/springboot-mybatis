46e3853fc73cf781448a4021d4833a4219fad1a6
lisi <lisi@163.com>
2020-09-05 08:56:29 +0800

    增加3333

--- src/main/java/com/atguigu/MyApplication.java
+++ src/main/java/com/atguigu/MyApplication.java
@@ -11,6 +11,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class MyApplication {
     public static void main(String[] args) {
         SpringApplication.run(MyApplication.class,args);
-        System.out.println("11111111111");
+        System.out.println("1111111111133333");
     }
 }
